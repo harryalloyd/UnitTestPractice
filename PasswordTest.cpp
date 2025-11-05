c24a87f2f619c1edae2a08c8989a527a6edd0295
Harrison Lloyd <harrisonalloyd@gmail.com>
2025-11-05 11:41:52 -0800

    added new tests

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,31 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, two_leading_letters)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aab");
+	ASSERT_EQ(2, actual);
+}
+
+TEST(PasswordTest, one_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("ab");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, upper_and_lower_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aAa");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, two_uppercase_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("AAab");
+	ASSERT_EQ(2, actual);
+}
